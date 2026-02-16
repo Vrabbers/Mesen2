61245343395129e3413926b32a8fac8f32e76e0f
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2026-02-16 13:32:09 -0300

    Update .NET, Avalonia and libraries

--- UI/Debugger/DebuggerDockFactory.cs
+++ UI/Debugger/DebuggerDockFactory.cs
@@ -267,4 +267,6 @@ namespace Mesen.Debugger
 public class MesenProportionalDockSplitter : DockBase, IProportionalDockSplitter
 {
 	//The regular ProportionalDockSplitter in Dock.Model.Mvvm.Controls inherits from DockableBase, which causes an exception when styles are applied
+	public bool CanResize { get => false; set => throw new NotImplementedException(); }
+	public bool ResizePreview { get => false; set => throw new NotImplementedException(); }
 }
